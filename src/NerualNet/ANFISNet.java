d428cf693d91eb85e4e94e259eec2e1a33fc7093
Adam Groves <agroves333@gmail.com>
2013-10-20 22:25:43 -0600

    Added some contructor code in ANFISNet

--- src/NerualNet/ANFISNet.java
+++ src/NerualNet/ANFISNet.java
@@ -9,6 +9,8 @@ public class ANFISNet implements Network {
 	private int numRules;
 	private int numOutputs;
 	
+	private char classifier;
+	
 	private double rate;
 	private double max;
 	
@@ -21,42 +23,48 @@ public class ANFISNet implements Network {
 	private Random rnd = new Random();
 	
 	
-	public ANFISNet(int inputs, int numLabels, int outputs, double rate, int numSamples, double[][] set){
+	public ANFISNet(int inputs, int numLabels, int outputs, double rate, char classifier, int numSamples, double[][] set){
 		
-		System.out.print(set.length);
+		System.out.print(inputs);
 		
 		this.numInputs = inputs;
-//		this.numCenters = ;
+		this.numCenters = inputs * numLabels;
+		this.numRules = (int) Math.pow(numLabels, inputs);
 		this.numLabels = numLabels;
 		this.numOutputs = outputs;
+		this.classifier = classifier;
 		this.rate = rate;
 		this.max = 0.0;
 		
 		this.premiseLayer = new PremiseNeuron[this.numCenters];
+		this.multiplierLayer = new MultiplierNeuron[this.numRules];
+		this.normalizationLayer = new NormalizerNeuron[this.numRules];
+		this.consequentLayer = new ConsequentNeuron[this.numRules];
 		this.output = new Neuron[this.numOutputs];
+		
 		
 		int index;
 		double[] input = new double[this.numInputs];
 		
 		// Initialize premise layer
-		for (int i = 0; i < this.numRules; i++){
-			this.premiseLayer[i] = new PremiseNeuron(this.numRules);
+		for (int i = 0; i < this.numCenters; i++){
+			this.premiseLayer[i] = new PremiseNeuron(1);
 		}
 		// Initialize multiplier layer
-		for (int i = 0; i < this.numCenters; i++){
-			this.multiplierLayer[i] = new MultiplierNeuron(this.numInputs);
+		for (int i = 0; i < this.numRules; i++){
+			this.multiplierLayer[i] = new MultiplierNeuron(this.numLabels);
 		}
 		// Initialize normalization layer
-		for (int i = 0; i < this.numCenters; i++){
-			this.normalizationLayer[i] = new NormalizerNeuron(this.numInputs);
+		for (int i = 0; i < this.numRules; i++){
+			this.normalizationLayer[i] = new NormalizerNeuron(this.numRules);
 		}
 		// Initialize consequent layer
-		for (int i = 0; i < this.numCenters; i++){
-			this.consequentLayer[i] = new ConsequentNeuron(this.numInputs);
+		for (int i = 0; i < this.numRules; i++){
+			this.consequentLayer[i] = new ConsequentNeuron(this.numInputs + 1);
 		}
 		// Initialize output nodes
 		for (int i = 0; i < this.numOutputs; i++){
-			this.output[i] = new Neuron(this.numCenters, 1);
+			this.output[i] = new Neuron(this.numRules, 1);
 		}
 		
 		this.setMaxD(set, numSamples);
@@ -73,6 +81,8 @@ public class ANFISNet implements Network {
 		}	
 	}
 	
+	
+	
 	private void setMaxD(double[][] set, int numSamples){
 		double d = 0.0;
 		
@@ -88,12 +98,15 @@ public class ANFISNet implements Network {
 	}
 	
 	
-	public void train(double[][] set, int numSamples, int epochs, char[] classes) {	
+	public void train(double[][] set, int numSamples, int epochs, char[] classes){		
 		int a = 0;
 		double wPrime;
+		double expected;
 		double[] input = new double[this.numInputs];
-		double[] expected = new double[this.numOutputs];
-		double[] hiddenOut = new double[this.numCenters];
+		double[] premiseOut = new double[this.numCenters];
+		double[] multiplierOut = new double[this.numRules];
+		double[] normalizerOut = new double[this.numRules];
+		double[] consequentOut = new double[this.numRules];
 		double[] error = new double[this.numOutputs];
 		double[] error2 = new double[this.numCenters];
 		double[] uPrime = new double[this.numInputs];
@@ -102,34 +115,77 @@ public class ANFISNet implements Network {
 		while (a < epochs){
 			for (int i = 0; i < numSamples; i++){
 				this.rate = (this.rate / 1.000001);
-				for (int j = this.numInputs; j < this.numInputs + this.numOutputs; j++){
-					expected[j - this.numInputs] = set[j][i];
+				if(this.classifier == classes[i])
+					expected = 1.0;
+				else
+					expected = 0.0;
+				
+				// Populate input from set
+				for (int k = 0; k < this.numInputs; k++){
+					input[k] = set[k][i];
+				}
+				
+				for (int j = 0; j < this.numCenters; j++){
+					for (int k = 0; k < this.numInputs; k++){
+						input[k] = set[k][i];
+					}
+					this.premiseLayer[j].activate(input);
+					premiseOut[j] = this.premiseLayer[j].getOutput();
 				}
 				
 				// Activate the premise nodes
 				for (int j = 0; j < this.numCenters; j++){
 					for (int k = 0; k < this.numInputs; k++){
 						input[k] = set[k][i];
-					}				
+					}
 					this.premiseLayer[j].activate(input);
-					hiddenOut[j] = this.premiseLayer[j].getOutput();
+					premiseOut[j] = this.premiseLayer[j].getOutput();
+				}
+				
+				
+				
+				// Activate the multiplier nodes
+				for (int j = 0; j < this.numCenters; j++){
+					for (int k = 0; k < this.numInputs; k++){
+						input[k] = set[k][i];
+					}
+					this.premiseLayer[j].activate(input);
+					premiseOut[j] = this.premiseLayer[j].getOutput();
+				}
+				
+				// Activate the normalizer nodes
+				for (int j = 0; j < this.numCenters; j++){
+					for (int k = 0; k < this.numInputs; k++){
+						input[k] = set[k][i];
+					}
+					this.premiseLayer[j].activate(input);
+					premiseOut[j] = this.premiseLayer[j].getOutput();
+				}
+				
+				// Activate the consequent nodes
+				for (int j = 0; j < this.numCenters; j++){
+					for (int k = 0; k < this.numInputs; k++){
+						input[k] = set[k][i];
+					}
+					this.premiseLayer[j].activate(input);
+					premiseOut[j] = this.premiseLayer[j].getOutput();
 				}
 				
 				// Activate the output node				
-				for (int j = 0; j < this.numOutputs; j++){
-					this.output[j].activate(hiddenOut);
-					error[j] = this.output[j].getOutput() - expected[j];
-				}
-				
-				Neuron n;
-				
-				for (int j = 0; j < this.numOutputs; j++){
-					n = this.output[j];
-					for (int k = 0; k < this.numCenters; k++){
-						wPrime = n.getWeight()[k] - this.rate * error[j] * hiddenOut[k];
-						n.setWeight(k, wPrime);
-					}
-				}
+//				for (int j = 0; j < this.numOutputs; j++){
+//					this.output[j].activate(hiddenOut);
+//					error[j] = this.output[j].getOutput() - expected;
+//				}
+//				
+//				Neuron n;
+//				
+//				for (int j = 0; j < this.numOutputs; j++){
+//					n = this.output[j];
+//					for (int k = 0; k < this.numCenters; k++){
+//						wPrime = n.getWeight()[k] - this.rate * error[j] * hiddenOut[k];
+//						n.setWeight(k, wPrime);
+//					}
+//				}
 				
 				double sum;
 				
@@ -157,38 +213,34 @@ public class ANFISNet implements Network {
 		}
 	}	
 	
-	public char process(double[][] set, int numSamples){
-		double totalE = 0.0;
+	public char process(double[][] set, int index){
 		double[] input = new double[this.numInputs];
-		double[] expected = new double[this.numOutputs];
-		double[] error = new double[this.numOutputs];
 		double[] hiddenOut = new double[this.numCenters];
+		double out = 0.0;
 		
-		
-		for (int i = 0; i < numSamples; i++){
-			
-			for (int j = 0; j < this.numInputs; j++){
-				input[j] = set[j][i];
-			}
-
-			for (int j = this.numInputs; j < this.numInputs + this.numOutputs; j++){
-				expected[j - this.numInputs] = set[j][i];
-			}
-			
-			for (int j = 0; j < this.numCenters; j++){				
-				this.premiseLayer[j].activate(input);
-				hiddenOut[j] = this.premiseLayer[j].getOutput();
-			}
-			
-			// Activate the output node			
-			for (int j = 0; j < this.numOutputs; j++){
-				this.output[j].activate(hiddenOut);
-				error[j] = this.output[j].getOutput() - expected[j];
-				totalE += (Math.abs(error[j]/expected[j]));
-			}
+		for (int j = 0; j < this.numInputs; j++){
+			input[j] = set[j][index];
 		}
-//		return ((totalE/numSamples)*100);
-		return 0;
+		
+		for (int j = 0; j < this.numCenters; j++){				
+			this.premiseLayer[j].activate(input);
+			hiddenOut[j] = this.premiseLayer[j].getOutput();
+		}
+		
+		// Activate the output node			
+		for (int j = 0; j < this.numOutputs; j++){
+			this.output[j].activate(hiddenOut);
+			if(this.output[j].getOutput() > 0.5)
+				out = 1.0;
+			else
+				out = 0.0;
+		}
+		
+		if(out == 1.0)
+			return (this.classifier);
+		else
+			return '!';
+		
 	}
 	
 }
